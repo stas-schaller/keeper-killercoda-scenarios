77c82deef0dd0b79d032de8b85de86858101805f
Max Ustinov <mustinov@keepersecurity.com>
2022-05-26 09:27:31 -0700

    Updating nodejs example to use ubuntu image and install nodejs

--- ksm-sdk-javascript/init.sh
+++ ksm-sdk-javascript/init.sh
@@ -1,3 +1,10 @@
 # #!/bin/bash
 # Make sure we are in the `ksm-sample-js` directory in CLI
 mkdir ksm-sample-js && cd ksm-sample-js
+
+# Install NodeJS
+
+sudo apt install -y curl
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+node --version
