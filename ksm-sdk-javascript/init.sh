d058d485af20de24ae54a2444c2c8c90732ef039
Max Ustinov <mustinov@keepersecurity.com>
2022-05-26 10:07:24 -0700

    fixing JS example

--- ksm-sdk-javascript/init.sh
+++ ksm-sdk-javascript/init.sh
@@ -2,7 +2,7 @@
 # Make sure we are in the `ksm-sample-js` directory in CLI
 mkdir ksm-sample-js && cd ksm-sample-js
 
-# Install NodeJS
+# Install NodeJS 16
 sudo apt-get update
 sudo apt-get install -y curl
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
