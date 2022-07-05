611ee1e29a47d19de30c46e2bc5505b53f731d4b
Max Ustinov <mustinov@keepersecurity.com>
2022-07-05 09:53:46 -0700

    added steps to install correct python

--- commander-cli/install-commander.sh
+++ commander-cli/install-commander.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-apt install python3-pip -y
-
-pip3 install pyOpenSSL --upgrade
-
-pip3 install keepercommander keeper-secrets-manager-core
+apt install python3-pip -y && pip3 install pyOpenSSL --upgrade && pip3 install keepercommander keeper-secrets-manager-core
