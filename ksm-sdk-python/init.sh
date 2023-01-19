6cbef5e5d35f321fe0596e09de6feb32850421e2
Max Ustinov <mustinov@keepersecurity.com>
2023-01-19 11:17:53 -0800

    fixed Python KSM SDK Exampale

--- ksm-sdk-python/init.sh
+++ ksm-sdk-python/init.sh
@@ -5,10 +5,12 @@ sudo apt-get update
 
 # install pypi manually, not through apt-get as it will give us some errors with the installed package
 
-sudo apt remove -y python3-pip
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip.py
-rm get-pip.py
+#sudo apt remove -y python3-pip
+#wget https://bootstrap.pypa.io/get-pip.py
+#sudo python3 get-pip.py
+#rm get-pip.py
+
+#apt install python3-pip -y && pip3 install pyOpenSSL --upgrade
 
 # Install KSM SDK
 python3 -m pip install -U keeper-secrets-manager-core
