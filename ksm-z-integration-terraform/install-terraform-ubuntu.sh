be3d38efd31128b31e87b6c140a06a2b0a55cc58
Max Ustinov <mustinov@keepersecurity.com>
2023-08-29 08:35:47 -0700

    Simplify Terraform installation on Ubuntu
    Removed the apt installation and several unneeded commands in the installation script for a more streamlined process. Replaced the method of Terraform installation by using Ubuntu's snap package instead. This reduces the amount of necessary steps to install Terraform, making the setup more efficient.

--- ksm-z-integration-terraform/install-terraform-ubuntu.sh
+++ ksm-z-integration-terraform/install-terraform-ubuntu.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Install apt
-wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_2.6.0ubuntu0.1_amd64.deb -O apt.deb
-sudo dpkg -i apt.deb
 # Install Terraform CLI in Ubuntu
 # Source: https://learn.hashicorp.com/tutorials/terraform/install-cli
-apt update -y
-# apt-get install -y curl
-# apt-get install -y gnupg 
-apt install -y software-properties-common
+#apt update -y
+## apt-get install -y curl
+## apt-get install -y gnupg
+#apt install -y software-properties-common
+#
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+#apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+#
+## Update to add the repository, and install the Terraform CLI.
+#apt update -y
+#apt install -y terraform
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - 
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
-# Update to add the repository, and install the Terraform CLI.
-apt update -y
-apt install -y terraform
-
+snap install terraform --classic
 # echo "Terraform was installed" >> /opt/.backgroundfinished
 
 cd ~/terraform-example
