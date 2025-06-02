8f6b164fc244711b90005f151dc64bc3e8c5835b
Max Ustinov <maksimu@users.noreply.github.com>
2025-06-02 16:56:34 -0700

    fix: ensure non-interactive Terraform install in Katacoda

--- ksm-z-integration-terraform/install-terraform-ubuntu.sh
+++ ksm-z-integration-terraform/install-terraform-ubuntu.sh
@@ -2,19 +2,9 @@
 
 # Install Terraform CLI in Ubuntu
 # Source: https://learn.hashicorp.com/tutorials/terraform/install-cli
-#apt update -y
-## apt-get install -y curl
-## apt-get install -y gnupg
-#apt install -y software-properties-common
-#
-#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-#apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-#
-## Update to add the repository, and install the Terraform CLI.
-#apt update -y
-#apt install -y terraform
+apt update -y && apt-get install -y curl gnupg software-properties-common && curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add - && apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && apt update -y && apt install -y terraform
 
-snap install terraform --classic
+# snap install terraform --classic
 # echo "Terraform was installed" >> /opt/.backgroundfinished
 
 mkdir ~/terraform-example && cd ~/terraform-example && touch main.tf
